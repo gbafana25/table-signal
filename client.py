f5246144d7c852995146192e5cb3befbfe6211e9
Gareth Moodley <garethkm25@gmail.com>
2021-03-24 09:55:45 -0500

    changed ready indicator

--- client.py
+++ client.py
@@ -1,14 +1,25 @@
 from microbit import *
 import radio
+import utime
 
 TABLE_NUM = 2
 radio.on()
 
+all_on = Image(
+    "99999:"
+    "99999:"
+    "99999:"
+    "99999:"
+    "99999"
+)
+
+
 def flash_display():
     while True:
-        display.scroll("READY")
+        display.show(all_on)
 
 while True:
     recv = radio.receive()
     if recv == str(TABLE_NUM):
         flash_display()
+
